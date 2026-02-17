d860f9fd27591acbd8a46591c8cd00134f6be356
Arjay Fuentes <arjay630@gmail.com>
2026-02-17 23:30:38 +0100

    updated formatiing and ordering 2

--- src/main/java/com/rjproj/reserba/repository/ItemRepository.java
+++ src/main/java/com/rjproj/reserba/repository/ItemRepository.java
@@ -3,6 +3,7 @@ package com.rjproj.reserba.repository;
 import com.rjproj.reserba.model.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
@@ -18,5 +19,15 @@ public interface ItemRepository extends JpaRepository <Item, UUID> {
 
     @Query("SELECT DISTINCT i.category FROM Item i WHERE i.category IS NOT NULL")
     List<String> findAllCategories();
+
+    @Query("SELECT i FROM Item i WHERE " +
+            "LOWER(i.name) LIKE LOWER(CONCAT('%', :name, '%')) " +
+            "AND i.category = :category")
+    List<Item> findByNameAndCategory(@Param("name") String name, @Param("category") String category);
+
+    @Query("SELECT i FROM Item i WHERE " +
+            "(:name IS NULL OR LOWER(i.name) LIKE LOWER(CONCAT('%', :name, '%'))) " +
+            "AND (:category IS NULL OR i.category = :category)")
+    List<Item> searchItems(@Param("name") String name, @Param("category") String category);
 
 }
